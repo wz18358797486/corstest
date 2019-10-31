07b8d0322befbbfe1e71626fb8b2433625207c83
admin <admin@qq.com>
2019-10-31 09:23:59 +0800

    text

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController("/" )
 public class TestController {
 
