67858af2b848a64d62ed91766c0dc8ea4750655a
Your Name <you@example.com>
2019-12-12 09:35:13 +0800

    yes

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,31 +1,49 @@
 package cn.wzvtc.soft;
 
-import org.omg.CORBA.ORB;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin
-@RestController("/" )
+@CrossOrigin(origins = {"http://127.0.0.1:5500"},allowCredentials = "true")
+@RestController
 public class TestController {
 
 
-    @RequestMapping(value="/data.json")
-    public Map<String,String> bookById(){
-        Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","苏圣扬");
-        resultMap.put("mynumber","28002090131");
-        return resultMap;
-    }
-    @RequestMapping(value="/login")
-    public Map<String,String> login(@RequestBody Map<String,String> map){
-        String password=map.get("password");
-        String number=map.get("number");
-        Map<String,String> resultMap=new HashMap<>();
-        if("28002090131".equals(number) && "28002090131".equals(password)){
-            resultMap.put("result","success");
+    @RequestMapping(value = "/userinfo")
+    public Map<String, String> userinfo(HttpServletRequest httpServletRequest) {
+
+        Map<String, String> resultMap = new HashMap<>();
+        //判断是否已经登陆,判断SESSION里面是否有我的用户名信息
+        if (httpServletRequest.getSession().getAttribute("loginnumber") != null) {
+            resultMap.put("myname", (String) httpServletRequest.getSession(true).getAttribute("loginnumber"));
+            resultMap.put("mynumber", (String) httpServletRequest.getSession(true).getAttribute("username"));
         }
         return resultMap;
     }
+
+    @RequestMapping(value = "/login")
+    public Map<String, String> login(@RequestBody Map<String, String> map, HttpServletRequest httpServletRequest) {
+        String password = map.get("password");
+        String number = map.get("number");
+        Map<String, String> resultMap = new HashMap<>();
+        if (number!=null && number.equals(password)) {
+            //将用户名存入到session中
+            httpServletRequest.getSession().setAttribute("loginnumber", number);
+            httpServletRequest.getSession().setAttribute("username", number);
+            resultMap.put("result", "success");
+        }
+        return resultMap;
+    }
+
+    @RequestMapping(value = "/logout")
+    public void logout(HttpServletRequest httpServletRequest) {
+        httpServletRequest.getSession().removeAttribute("loginnumber");
+    }
+
 }
